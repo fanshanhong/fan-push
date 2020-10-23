1dd22a50d7520bf7a3d1654f03206055fa1dc1ba
fanshanhong <443710231@qq.com>
2020-10-23 14:13:54 +0800

    <feature>:空项目提交
    第一次空项目提交,添加了Netty相关依赖

---
+++ app/src/main/java/com/fan/push/server/PushServer.java
@@ -0,0 +1,14 @@
+package com.fan.push.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class PushServer {
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
