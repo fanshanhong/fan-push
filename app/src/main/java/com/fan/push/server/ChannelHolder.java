43109839caca2058136f451db2e1dcf4772fd13c
fanshanhong <443710231@qq.com>
2020-11-11 17:03:46 +0800

    <feature>:Channel管理器:ChannelHolder
    Channel管理器:ChannelHolder

--- app/src/main/java/com/fan/push/server/ChannelHolder.java
+++ app/src/main/java/com/fan/push/server/ChannelHolder.java
@@ -20,6 +20,7 @@ public class ChannelHolder {
 
     // https://www.cnblogs.com/liangshu/p/12459657.html
 
+    // userId <==> channel 映射关系
     private final ConcurrentHashMap<String, Channel> channelMap = new ConcurrentHashMap<>();
 
     /**
@@ -35,30 +36,39 @@ public class ChannelHolder {
     }
 
     /**
-     * 上线一个用户
+     * 上线一个用户, 要将这个Channel 加入到 ChannelMap 中进行管理
      *
      * @param channel
      * @param userId
      */
     public void online(Channel channel, String userId) {
-        //先判断用户是否在系统中登录?
-        //这部分代码个人实现,参考上面redis中的验证
+        // 先判断用户是否在系统中登录?
 
         this.channelMap.put(userId, channel);
         AttributeKey<String> key = AttributeKey.valueOf("user");
         channel.attr(key).set(userId);
     }
 
+    /**
+     * 下线一个用户, 将Channel 从 ChannelMap 中移除
+     *
+     * @param channel
+     */
     public void offline(Channel channel) {
+
         String userId = null;
+
+        // 先找到channel 对应的 userId
         AttributeKey<String> key = AttributeKey.valueOf("user");
         if (channel.hasAttr(key) || channel.attr(key).get() != null) {
             userId = channel.attr(key).get();
         }
 
         if (userId == null) {
-            throw new IllegalArgumentException("userId cannot is null");
+            System.out.println("no userId with this channel:" + channel.toString());
+            return;
         }
+
         if (channelMap.containsKey(userId)) {
             channelMap.remove(userId);
         }
