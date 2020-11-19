ff4abc2f835f809936583a533bbd17056e0e70b2
fanshanhong <443710231@qq.com>
2020-11-19 11:32:58 +0800

    <feature>:Log和堆栈信息工具类
    Log和堆栈信息工具类

--- app/src/main/java/com/fan/push/util/LoggerUtil.java
+++ app/src/main/java/com/fan/push/util/LoggerUtil.java
@@ -3,6 +3,12 @@ package com.fan.push.util;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
+/**
+ * @Description: Log 工具类
+ * @Author: fan
+ * @Date: 2020-10-19 11:19
+ * @Modify:
+ */
 public class LoggerUtil {
     public static final InternalLogger logger = InternalLoggerFactory.getInstance("fan-push-logger=======");
 }
