9c72ae1f7d45b32db6cebf33756698e3e308109d
fanshanhong <443710231@qq.com>
2020-11-19 11:21:45 +0800

    <feature>:Gson工具类
    Gson工具类

--- app/src/main/java/com/fan/push/util/GsonUtil.java
+++ app/src/main/java/com/fan/push/util/GsonUtil.java
@@ -3,14 +3,18 @@ package com.fan.push.util;
 import com.google.gson.Gson;
 
 /**
- * Created by fan on 2016/8/11.
+ * @Description: Gson 工具类, 用于获取一个 Gson 对象
+ * @Author: fan
+ * @Date: 2020-10-19 11:19
+ * @Modify:
  */
 public class GsonUtil {
 
     private static Gson gson = null;
 
-    public static Gson getInstance(){
-        if (gson == null){
+    public static Gson getInstance() {
+
+        if (gson == null) {
             gson = new Gson();
         }
         return gson;
